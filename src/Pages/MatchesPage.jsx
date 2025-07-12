6d5985bdf46a4f6794f89a711d92dbc8c8b994b4
Adeola <adeolaojuade1@gmail.com>
2025-07-11 20:48:15 -0700

    fixed bugs with navbar width and height, added placeholders for book results, and started button implementation

--- src/Pages/MatchesPage.jsx
+++ src/Pages/MatchesPage.jsx
@@ -1,9 +1,29 @@
-
+import './MatchesPage.css'
 
 const MatchesPage = () => {
   return (
-    <div className="matches-page">
-      <h1>Here’s what we got. You can also refine your search!</h1>
+    <div className="matches-page" >
+      <div className="header">
+        <h1>Here’s what we got. You can also refine your search!</h1>
+        <p>Click any book to read a synopsis. Hover over the banners to read more about each match.</p>
+      </div>
+
+      <div className="results">
+        <h2>Top Matches</h2>
+        <div className="result-books">
+          <div className="book">Book One</div>
+          <div className="book">Book Two</div>
+          <div className="book">Book Three</div>
+          <div className="book">Book Four</div>
+        </div>
+      </div>
+
+      <div className="search-buttons">
+        <div className="search-again">Search Again</div>
+        <div className="personalized-search">Get More Personalized Results</div>
+      </div>
+      
+    
     </div>
   )
 }
